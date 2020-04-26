5715f9082e9862496f691e882d248f2eea21d4ba
georgikoemdzhiev <koemdjiev@gmail.com>
2020-04-26 20:52:51 +0100

    Added alive/dead counters

--- Main.cs
+++ Main.cs
@@ -24,8 +24,8 @@ public class Main : Node2D
 
     private void VisitCells()
     {
-        var aliveList = new List<Cell>();
-        var deadList = new List<Cell>();
+        var aliveNeighboursCount = 0;
+        var deadNeighboursCount = 0;
         for (int x = 0; x < _grid.CellGrid.GetLength(0); x++)
         {
             for (int y = 0; y < _grid.CellGrid.GetLength(1); y++)
@@ -43,14 +43,44 @@ public class Main : Node2D
                     var sw = _grid.CellGrid[x - 1, y + 1];
                     var se = _grid.CellGrid[x + 1, y + 1];
 
+                    if (n is Cell north)
+                        if (north.IsAlive) aliveNeighboursCount++; else deadNeighboursCount++;
+
+                    if (e is Cell east)
+                        if (east.IsAlive) aliveNeighboursCount++; else deadNeighboursCount++;
+
+                    if (s is Cell south)
+                        if (south.IsAlive) aliveNeighboursCount++; else deadNeighboursCount++;
+
+                    if (w is Cell west)
+                        if (west.IsAlive) aliveNeighboursCount++; else deadNeighboursCount++;
+
+                    if (nw is Cell northWest)
+                        if (northWest.IsAlive) aliveNeighboursCount++; else deadNeighboursCount++;
+
+                    if (ne is Cell northEast)
+                        if (northEast.IsAlive) aliveNeighboursCount++; else deadNeighboursCount++;
+
+                    if (sw is Cell southWest)
+                        if (southWest.IsAlive) aliveNeighboursCount++; else deadNeighboursCount++;
+
+                    if (se is Cell southEest)
+                        if (southEest.IsAlive) aliveNeighboursCount++; else deadNeighboursCount++;
+
                     GD.Print($"Visiting: {x},{y}");
                 }
-                catch (Exception _)
+                catch (Exception exception)
                 {
+                    GD.Print(exception.Message);
                     // Here I am using Try-Catch to avoid having 8 if statements to check for edge cells
                 }
 
+                // reset counts for next cell
+                aliveNeighboursCount = 0;
+                deadNeighboursCount = 0;
+
             }
+
         }
     }
 }
